e9649037c7ae0e8294cfe3ff7a8e0c4d3e34ea0f
Ubuntu <ubuntu@ip-172-31-52-38.ec2.internal>
2024-09-23 14:21:55 +0000

    check

--- configuration/after_install.sh
+++ configuration/after_install.sh
@@ -1,36 +1,43 @@
 #!/usr/bin/env bash
 
+
 cd /home/ubuntu/g2020wa15340
 
 # activate virtual environment
-python3 -m venv venv
+sudo python3 -m venv venv
 source venv/bin/activate
 
 echo "install requirements.txt"
-venv/bin/pip install -r /home/ubuntu/g2020wa15340/requirements.txt
+sudo venv/bin/pip install -r /home/ubuntu/g2020wa15340/requirements.txt
 
 echo "collecting all static files"
 cd /home/ubuntu
-venv/bin/python3 g2020wa15340/manage.py collectstatic
-venv/bin/python3 g2020wa15340/manage.py makemigrations
-venv/bin/python3 g2020wa15340/manage.py migrate
+sudo venv/bin/python3 g2020wa15340/manage.py collectstatic
+sudo venv/bin/python3 g2020wa15340/manage.py makemigrations
+sudo venv/bin/python3 g2020wa15340/manage.py migrate
 
 echo "Starting apache server"
-systemctl start apache2
-systemctl enable apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
 
 cd /home/ubuntu/
 
 echo "Giving permissions"
-cp configuration/django-apache.conf /etc/apache2/sites-available/
-chown :www-data g2020wa15340/
-chown :www-data g2020wa15340/media
-chown :www-data g2020wa15340/media/*
-chown :www-data g2020wa15340/media/profile_pics/*
-chown :www-data g2020wa15340/static
-chmod 755 $(pwd)
-chown :www-data g2020wa15340/db.sqlite3
-chmod 755 g2020wa15340/db.sqlite3
+sudo cp configuration/django-apache.conf /etc/apache2/sites-available/
+# sudo chown :www-data g2020wa15340/
+# sudo chown :www-data g2020wa15340/media
+# sudo chown :www-data g2020wa15340/media/*
+# sudo chown :www-data g2020wa15340/media/profile_pics/*
+# sudo chown :www-data g2020wa15340/static
+# sudo chown :www-data /home/ubuntu
+sudo chown :www-data /home/ubuntu/g2020wa15340
+sudo chown :www-data /home/ubuntu/g2020wa15340/*
+sudo chown :www-data /home/ubuntu/g2020wa15340/static/*
+sudo chown :www-data /home/ubuntu/g2020wa15340/media/*
+sudo chown :www-data /home/ubuntu/g2020wa15340/media/*/*
+sudo chmod 755 $(pwd)
+sudo chown :www-data g2020wa15340/db.sqlite3
+sudo chmod 755 g2020wa15340/db.sqlite3
 sudo a2ensite django-apache.conf
 sudo systemctl reload apache2
 sudo a2dissite 000-default.conf
@@ -38,5 +45,3 @@ sudo systemctl reload apache2
 sudo systemctl restart apache2
 
 deactivate
-
-
