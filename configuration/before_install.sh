0856847c0391b3bb1e68fd35c62501e821df69e1
Ubuntu <ubuntu@ip-172-31-52-38.ec2.internal>
2024-09-25 16:12:31 +0000

    Adding

--- configuration/before_install.sh
+++ configuration/before_install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 # # clean codedeploy-agent files for a fresh install
 # sudo rm -rf /home/ubuntu/instal
@@ -28,6 +28,7 @@ TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-me
 
 curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4 > /tmp/publicip.txt
 sudo chmod 777 /tmp/publicip.txt
-
+export IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+echo "IP from export = $IP"
 sudo python3 set_env.py
-echo $IP
+echo "echo from IP 2 $IP"
