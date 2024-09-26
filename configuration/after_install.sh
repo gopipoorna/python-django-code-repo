973a7d2df52d05189079e465c6247eb48522984b
gopipoorna <ubuntu@ip-172-31-52-38.ec2.internal>
2024-09-26 07:01:41 +0000

    Adding

--- configuration/after_install.sh
+++ configuration/after_install.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# Grab an IMDSv2 token
+TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+
+curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4 > /tmp/public_ip.txt
+
+sudo chmod 777 /tmp/public_ip.txt
+
+cat /tmp/public_ip.txt
+
+sudo chmod 777 /home/ubuntu/g2020wa15340/.env
+
+sudo python3 /home/ubuntu/configuration/set_ip.py
+
 cd /home/ubuntu/g2020wa15340
 
 # activate virtual environment
