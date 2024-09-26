75dce7def17f14c81b94ced93b4b84470094bb89
gopipoorna <ubuntu@ip-172-31-52-38.ec2.internal>
2024-09-26 06:58:02 +0000

    Adding variables

--- configuration/set_ip.py
+++ configuration/set_ip.py
@@ -3,7 +3,7 @@
 import re
 import os
 
-file = open("/home/ubuntu/environment/python-django-code-repo/g2020wa15340/.env", "r+")
+file = open("/home/ubuntu/g2020wa15340/.env", "r+")
 file2 = open("/tmp/public_ip.txt", "r+")
 new_ip=file2.read()
 val = file.read()
@@ -15,8 +15,8 @@ print(new_val)
 file.close()
 file2.close()
 
-os.remove("/home/ubuntu/environment/python-django-code-repo/g2020wa15340/.env")
+os.remove("/home/ubuntu/g2020wa15340/.env")
 
-file3 = open("/home/ubuntu/environment/python-django-code-repo/g2020wa15340/.env", "w")
+file3 = open("/home/ubuntu/g2020wa15340/.env", "w")
 file3.write(new_val.strip())
 file3.close()
